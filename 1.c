e088b2f23114375686d88bb63ef379bf4c701eb9
Ox <oxtookbronze@gmail.com>
2021-03-06 18:32:35 -0800

    got .78 working

--- 1.c
+++ 1.c
@@ -8,13 +8,20 @@ void main ()
 	int i; double x, pi, sum;
 	step = 1.0/(double) num_steps;
 	double start = omp_get_wtime();
+#pragma omp parallel
+	{
 	int id = omp_get_thread_num();
 	int size = omp_get_num_threads();
+
 	for (i=id;i< num_steps; i+=size){
+#pragma omp critical
+		{
 		x = (i+0.5)*step;
 		sum = sum + 4.0/(1.0+x*x);
+		}
 		
 	}
+}
 	double end = omp_get_wtime();
 	pi = step * sum;
 	printf("%f : %f\n",pi,end-start);
