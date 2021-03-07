cf79eaee9871d8a361703fa4b9c9696505a5097e
Ox <oxtookbronze@gmail.com>
2021-03-06 16:28:01 -0800

    start

---
+++ 1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "omp.h"
+
+static long num_steps = 100000;
+double step;
+void main ()
+{
+	int i; double x, pi, sum = 0.0;
+	step = 1.0/(double) num_steps;
+	double start = omp_get_wtime();
+	int id = omp_get_thread_num();
+	int size = omp_get_num_threads();
+	for (i=id;i< num_steps; i+=size){
+		x = (i+0.5)*step;
+		sum = sum + 4.0/(1.0+x*x);
+	}
+	double end = omp_get_wtime();
+	pi = step * sum;
+	printf("%f : %f\n",pi,end-start);
+}
